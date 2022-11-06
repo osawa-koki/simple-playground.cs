87e9bc5d6907e939ce9d869bc5b7ba63dcd024aa
osawa-koki <github@osawa.dev>
2022-11-06 20:39:27 +0900

    クラス初期化。

--- 12.operation_on_class/Program.cs
+++ 12.operation_on_class/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    // クラス初期化時に、クラスプロパティを初期化することもできます。
+    // この場合には「{...}」内で初期値を指定します。
+
+    var osawa = new Person()
+    {
+      Name = "osawa",
+      Age = 23
+    };
+    osawa.SayHello();
+    
+    // >>> Hello, my name is osawa. I'm 23 years old.
+
+    // ☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
+    // ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆
+    // ☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
+    // ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆
+    // ☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
+
+    
+
+
+  }
+}
+
+
+
+
+internal class Person
+{
+  public string Name;
+  public int Age;
+
+  public void SayHello()
+  {
+    Console.WriteLine($"Hello my name is {Name} and I am {Age} years old.");
+  }
+}
+
