4940263520d3ba34f7d6635c0751138304cc115a
osawa-koki <github@osawa.dev>
2022-11-12 21:28:08 +0900

    ビット演算子。

--- 19.bit_operation/Program.cs
+++ 19.bit_operation/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// ここでは使わなそうで、よく意外と使う、ビット演算子を紹介します。
+
+{
+  // ビット演算子には以下の種類があります。
+  // &  : 論理積
+  // |  : 論理和
+  // ^  : 排他的論理和
+  // ~  : 否定
+  // << : 左シフト
+  // >> : 右シフト
+  
+  const int a = 31;
+  const int b = 15;
+  
+  Console.WriteLine(a & b); // 15
+  Console.WriteLine(a | b); // 31
+  Console.WriteLine(a ^ b); // 16
+  Console.WriteLine(~a); // -32
+  Console.WriteLine(a << 1); // 62
+  Console.WriteLine(a >> 1); // 15
+}
+
