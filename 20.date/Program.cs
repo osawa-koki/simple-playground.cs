50a175f24a7c7dcc7cdef8bd669b57337afc8f29
osawa-koki <github@osawa.dev>
2022-11-12 23:25:45 +0900

    DateTime.***

--- 20.date/Program.cs
+++ 20.date/Program.cs
@@ -228,3 +228,180 @@ using System.Globalization;
   // >>> コンソール
   // dt.ToString("yyyy/MM/dd HH:mm:ss.fff") -> 1998/10/25 12:34:56.789
 }
+{
+  // DateTime型のデータから、それぞれの構成要素を取得することができます。
+  // 以下のプロパティを使用します。
+  // Year : 年
+  // Month : 月
+  // Day : 日
+  // Hour : 時間
+  // Minute : 分
+  // Second : 秒
+  // Millisecond : ミリ秒
+  // DayOfWeek : 曜日
+  // DayOfYear : 1月1日からの経過日数
+
+  DateTime dt = new(1998, 10, 25, 12, 34, 56, 789);
+
+  Console.WriteLine($"dt.Year -> {dt.Year}");
+
+  // >>> コンソール
+  // dt.Year -> 1998
+
+  Console.WriteLine($"dt.Month -> {dt.Month}");
+
+  // >>> コンソール
+  // dt.Month -> 10
+
+  Console.WriteLine($"dt.Day -> {dt.Day}");
+
+  // >>> コンソール
+  // dt.Day -> 25
+
+  Console.WriteLine($"dt.Hour -> {dt.Hour}");
+
+  // >>> コンソール
+  // dt.Hour -> 12
+
+  Console.WriteLine($"dt.Minute -> {dt.Minute}");
+
+  // >>> コンソール
+  // dt.Minute -> 34
+
+  Console.WriteLine($"dt.Second -> {dt.Second}");
+
+  // >>> コンソール
+  // dt.Second -> 56
+
+  Console.WriteLine($"dt.Millisecond -> {dt.Millisecond}");
+
+  // >>> コンソール
+  // dt.Millisecond -> 789
+
+  Console.WriteLine($"dt.DayOfWeek -> {dt.DayOfWeek}");
+
+  // >>> コンソール
+  // dt.DayOfWeek -> Friday
+
+  Console.WriteLine($"dt.DayOfYear -> {dt.DayOfYear}");
+
+  // >>> コンソール
+  // dt.DayOfYear -> 298
+
+  // ===== ===== ===== ===== =====
+  // ちなみに、曜日は、以下のように取得することもできます。
+  // ===== ===== ===== ===== =====
+
+  Console.WriteLine($"dt.ToString(\"ddd\") -> {dt.ToString("ddd")}");
+
+  // >>> コンソール
+  // dt.ToString("ddd") -> Fri
+
+  Console.WriteLine($"dt.ToString(\"dddd\") -> {dt.ToString("dddd")}");
+
+  // >>> コンソール
+  // dt.ToString("dddd") -> Friday
+
+  // ===== ===== ===== ===== =====
+
+  // もっと簡単に!
+
+  Console.WriteLine($"dt.ToString(\"ddd\") -> {dt:ddd}");
+
+  // >>> コンソール
+  // dt.ToString("ddd") -> Fri
+
+  Console.WriteLine($"dt.ToString(\"dddd\") -> {dt:dddd}");
+
+  // >>> コンソール
+  // dt.ToString("dddd") -> Friday
+
+  // ===== ===== ===== ===== =====
+
+  // 僕は金曜日に生まれたらしいです。
+  // 華金ですね♪
+  // 僕が生まれた時代に華金って言葉があったのかは知りませんが、、、
+}
+{
+  // 日付型の比較
+  // DateTime型のデータは、比較演算子を使用して比較することができます。
+  // 以下の演算子を使用します。
+  // == : 等しい
+  // != : 等しくない
+  // < : より小さい
+  // > : より大きい
+  // <= : 以下
+  // >= : 以上
+
+  DateTime dt1 = new(1998, 10, 25, 12, 34, 56, 789);
+  DateTime dt2 = new(1998, 10, 25, 12, 34, 56, 789);
+
+  Console.WriteLine($"dt1 == dt2 -> {dt1 == dt2}");
+
+  // >>> コンソール
+  // dt1 == dt2 -> True
+
+  Console.WriteLine($"dt1 != dt2 -> {dt1 != dt2}");
+
+  // >>> コンソール
+  // dt1 != dt2 -> False
+
+  Console.WriteLine($"dt1 < dt2 -> {dt1 < dt2}");
+
+  // >>> コンソール
+  // dt1 < dt2 -> False
+
+  Console.WriteLine($"dt1 > dt2 -> {dt1 > dt2}");
+
+  // >>> コンソール
+  // dt1 > dt2 -> False
+
+  Console.WriteLine($"dt1 <= dt2 -> {dt1 <= dt2}");
+
+  // >>> コンソール
+  // dt1 <= dt2 -> True
+
+  Console.WriteLine($"dt1 >= dt2 -> {dt1 >= dt2}");
+
+  // >>> コンソール
+  // dt1 >= dt2 -> True
+
+  // ちなみに、DateTime型のデータは、以下のように比較することもできます。
+  // ただし、この方法は、DateTime型のデータを、long型に変換してから比較しているので、
+  // あまり推奨されません。
+
+  Console.WriteLine($"dt1.Ticks == dt2.Ticks -> {dt1.Ticks == dt2.Ticks}");
+
+  // >>> コンソール
+  // dt1.Ticks == dt2.Ticks -> True
+
+  // ちなみに、Ticksは、1970年1月1日0時0分0秒からの経過時間を100ナノ秒単位で表した値です。
+  // つまり、DateTime型のデータをlong型に変換すると、Ticksの値が取得できます。
+  // 以下のように、Ticksの値を取得することができます。
+
+  Console.WriteLine($"dt1.Ticks -> {dt1.Ticks}");
+
+  // >>> コンソール
+  // dt1.Ticks -> 630449156967890000
+
+  // これを、100ナノ秒単位で表した値に変換すると、以下のようになります。
+
+  Console.WriteLine($"dt1.Ticks / 10000000 -> {dt1.Ticks / 10000000}");
+
+  // >>> コンソール
+  // dt1.Ticks / 10000000 -> 63044915696
+
+  // これを、秒単位で表した値に変換すると、以下のようになります。
+
+  Console.WriteLine($"dt1.Ticks / 10000000 / 60 / 60 / 24 -> {dt1.Ticks / 10000000 / 60 / 60 / 24}");
+
+  // >>> コンソール
+  // dt1.Ticks / 10000000 / 60 / 60 / 24 -> 729686
+
+  // これを、日単位で表した値に変換すると、以下のようになります。
+
+  Console.WriteLine($"dt1.Ticks / 10000000 / 60 / 60 / 24 / 365 -> {dt1.Ticks / 10000000 / 60 / 60 / 24 / 365}");
+
+  // >>> コンソール
+  // dt1.Ticks / 10000000 / 60 / 60 / 24 / 365 -> 1999
+}
